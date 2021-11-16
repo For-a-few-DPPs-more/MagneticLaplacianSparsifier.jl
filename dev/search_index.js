5c5066c209a36bcc41c942b75aca4b9dd48155c7
Documenter.jl <documenter@juliadocs.github.io>
2021-11-16 13:35:58 +0000

    build based on 03af947

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MagneticLaplacianSparsifier","category":"page"},{"location":"#MagneticLaplacianSparsifier","page":"Home","title":"MagneticLaplacianSparsifier","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MagneticLaplacianSparsifier.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MagneticLaplacianSparsifier]","category":"page"}]
+}
