5e25633ece4293073c161416eb04f3420e032414
Michaël Fanuel <mrfanuel@hotmail.fr>
2021-11-17 14:40:45 +0100

    improve nb

--- src/laplacians.jl
+++ src/laplacians.jl
@@ -1,9 +1,30 @@
 
-function magneticIncidence(compGraph)
+function spMagneticIncidence(compGraph)
     n = nv(compGraph)
     m = ne(compGraph)
 
     B = spzeros(Complex,n,m)
+    edges_compGraph = edges(compGraph)
+
+    ind_e = 0
+    for e in edges_compGraph
+        ind_e += 1
+        u = src(e)
+        v = dst(e)
+        angle = get_edge_property(compGraph, e, :angle)
+        B[u, ind_e] = exp(-0.5 * angle*im)
+        B[v, ind_e] = -exp(0.5 * angle*im)
+    end
+
+    return B
+end
+
+
+function magneticIncidence(compGraph)
+    n = nv(compGraph)
+    m = ne(compGraph)
+
+    B = zeros(Complex,n,m)
     edges_compGraph = edges(compGraph)
 
     ind_e = 0
