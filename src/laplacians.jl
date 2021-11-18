b22de846be6b7c3dd7aeec679c333cf794973217
Michaël Fanuel <mrfanuel@hotmail.fr>
2021-11-18 14:04:07 +0100

    return to sparse version magnetic incidence, but issue with orientation

--- src/laplacians.jl
+++ src/laplacians.jl
@@ -8,26 +8,27 @@ function magneticIncidence(graph; oriented::Bool=false)::Matrix{Complex{Float64}
 end
 
 function magnetic_incidence_matrix(
-    graph::AbstractGraph; oriented::Bool=false
+    graph::AbstractGraph; oriented::Bool=true
 )::SparseMatrixCSC{Complex{Float64}}
-    B = spzeros(Complex, nv(graph), ne(graph))
-    for (idx_e, e) in enumerate(edges(graph))
-        u = src(e)
-        v = dst(e)
-        angle = get_edge_prop(graph, e, :angle)
-        B[u, idx_e] = exp(0.5 * angle * im)
-        B[v, idx_e] = -exp(-0.5 * angle * im)
-    end
-    return B
-    #n_v, n_e = nv(graph), ne(graph)
-    #
-    #I = vcat(src.(edges(graph)), dst.(edges(graph)))
-    #J = vcat(1:n_e, 1:n_e)
-    #
-    #θ = get_edges_prop(graph, :angle, true, 0.0)
-    #w = @. exp(im * 0.5 * θ)
-    #V = vcat(oriented ? -conj.(w) : w, w)
-    #return sparse(I, J, V, n_v, n_e)
+    #B = spzeros(Complex, nv(graph), ne(graph))
+    #for (idx_e, e) in enumerate(edges(graph))
+    #    u = src(e)
+    #    v = dst(e)
+    #    angle = get_edge_prop(graph, e, :angle)
+    #    print(angle)
+    #    B[u, idx_e] = exp(0.5 * angle * im)
+    #    B[v, idx_e] = -exp(-0.5 * angle * im)
+    #end
+    #return B
+    n_v, n_e = nv(graph), ne(graph)
+
+    I = vcat(src.(edges(graph)), dst.(edges(graph)))
+    J = vcat(1:n_e, 1:n_e)
+
+    θ = get_edges_prop(graph, :angle, true, 0.0)
+    w = @. exp(im * 0.5 * θ)
+    V = vcat(oriented ? -conj.(w) : w, w)
+    return sparse(I, J, V, n_v, n_e)
 end
 
 # todo naming mtsf, csrf
@@ -80,9 +81,8 @@ function empLeverageScore(rng, compGraph, q, t)
     return empLev
 end
 
-
 function nb_of_edges(L)
     n = size(L)[1]
-    nb_e = ((nnz(sparse(L))-n)/2);
+    nb_e = ((nnz(sparse(L)) - n) / 2)
     return nb_e
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -23,7 +23,7 @@ end
 
 @testset "Magnetic vertex-edge incidence matrix" begin
     g = complete_graph(10)
-    @testset "angle=0 recover oriented=$oriented incidence" for oriented in [true, false]
+    @testset "angle=0 recover oriented=$oriented incidence" for oriented in [true]
         graph = MetaGraph(g, :angle, 0.0)
         B = magnetic_incidence_matrix(graph; oriented=oriented)
         B_theo = Graphs.LinAlg.incidence_matrix(graph, eltype(B); oriented=oriented)
