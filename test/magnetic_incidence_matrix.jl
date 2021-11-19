656a70d0dcbd241609a719a169472a01d19da7a4
Michaël Fanuel <mrfanuel@hotmail.fr>
2021-11-19 21:32:54 +0100

    came back to basic mun and ero graph models, since new versions not ok. Correponding test defined and passed by basic version.

--- test/magnetic_incidence_matrix.jl
+++ test/magnetic_incidence_matrix.jl
@@ -6,4 +6,35 @@
         B_theo = Graphs.LinAlg.incidence_matrix(graph, eltype(B); oriented=oriented)
         @test B == B_theo
     end
+
+    n = 100
+    p = 0.5
+    eta = 0.0
+    rng = getRNG()
+
+    @testset "Basic MUN: check least eigenvalue is zero if ranking trivial" begin
+        meta_g = gen_graph_mun_basic(n, p, eta)
+        B = magnetic_incidence(meta_g)
+        L = B * B'
+        @test abs(minimum(eigvals(L))) < 1e-12
+    end
+    # @testset "ReFactored MUN: check least eigenvalue is zero if ranking trivial" begin
+    #     meta_g = gen_graph_mun(rng, n, p, eta)
+    #     B = magnetic_incidence(meta_g)
+    #     L = B * B'
+    #     @test abs(minimum(eigvals(L))) < 1e-12
+    # end
+
+    @testset "Basic ERO: check least eigenvalue is zero if ranking trivial" begin
+        meta_g = gen_graph_ero_basic(n, p, eta)
+        B = magnetic_incidence(meta_g)
+        L = B * B'
+        @test abs(minimum(eigvals(L))) < 1e-12
+    end
+    # @testset "ReFactored ERO: check least eigenvalue is zero if ranking trivial" begin
+    #     meta_g = gen_graph_ero(rng, n, p, eta)
+    #     B = magnetic_incidence(meta_g)
+    #     L = B * B'
+    #     @test abs(minimum(eigvals(L))) < 1e-12
+    # end
 end
