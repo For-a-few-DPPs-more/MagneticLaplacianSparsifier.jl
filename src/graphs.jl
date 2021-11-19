704a5bfa778b72333c98628af25a627b05e1389a
Michaël Fanuel <mrfanuel@hotmail.fr>
2021-11-19 21:07:18 +0100

    restore basic versions

--- src/graphs.jl
+++ src/graphs.jl
@@ -1,70 +1,70 @@
 
-# function gen_graph_mun(n, p, eta)
-#     # Following M. Cucuringu to build comparison graph
-#     # SYNC-RANK: ROBUST RANKING, CONSTRAINED RANKING AND RANK AGGREGATION VIA EIGENVECTOR AND SDP SYNCHRONIZATION
-#     # Multiplicative Uniform Noise model
-#     g = Graph(n)
-#     meta_g = MetaGraph(g, :angle, 0.0)
+function gen_graph_mun_basic(n, p, eta)
+    # Following M. Cucuringu to build comparison graph
+    # SYNC-RANK: ROBUST RANKING, CONSTRAINED RANKING AND RANK AGGREGATION VIA EIGENVECTOR AND SDP SYNCHRONIZATION
+    # Multiplicative Uniform Noise model
+    g = Graph(n)
+    meta_g = MetaGraph(g, :angle, 0.0)
 
-#     # ranking
-#     r = collect(1:n)
+    # ranking
+    r = collect(1:n)
 
-#     for i in r
-#         for j in r
-#             if i < j
-#                 if rand(1)[1] < p
-#                     e = [i j]
-#                     edges = consecutive_pairs(e)
-#                     add_edges_from!(meta_g, edges)
+    for i in r
+        for j in r
+            if i < j
+                if rand(1)[1] < p
+                    e = [i j]
+                    edges = consecutive_pairs(e)
+                    add_edges_from!(meta_g, edges)
 
-#                     err = eta * rand(1)[1]
-#                     a = pi * (i - j) * (1 + err) / (n - 1)
+                    err = eta * rand(1)[1]
+                    a = pi * (i - j) * (1 + err) / (n - 1)
 
-#                     for e in edges
-#                         set_prop!(meta_g, Edge(e), :angle, a)
-#                     end
-#                 end
-#             end
-#         end
-#     end
+                    for e in edges
+                        set_prop!(meta_g, Edge(e), :angle, a)
+                    end
+                end
+            end
+        end
+    end
 
-#     return meta_g
-# end
+    return meta_g
+end
 
-# function gen_graph_ero(n, p, eta)
-#     # Following M. Cucuringu to build comparison graph
-#     # SYNC-RANK: ROBUST RANKING, CONSTRAINED RANKING AND RANK AGGREGATION VIA EIGENVECTOR AND SDP SYNCHRONIZATION
-#     # Erdos-Renyi Outliers model
-#     g = Graph(n)
-#     meta_g = MetaGraph(g, :angle, 0.0)
+function gen_graph_ero_basic(n, p, eta)
+    # Following M. Cucuringu to build comparison graph
+    # SYNC-RANK: ROBUST RANKING, CONSTRAINED RANKING AND RANK AGGREGATION VIA EIGENVECTOR AND SDP SYNCHRONIZATION
+    # Erdos-Renyi Outliers model
+    g = Graph(n)
+    meta_g = MetaGraph(g, :angle, 0.0)
 
-#     # ranking
-#     r = collect(1:n)
+    # ranking
+    r = collect(1:n)
 
-#     for i in r
-#         for j in r
-#             if i < j
-#                 if rand(1)[1] < p
-#                     e = [i j]
-#                     edges = consecutive_pairs(e)
-#                     add_edges_from!(meta_g, edges)
+    for i in r
+        for j in r
+            if i < j
+                if rand(1)[1] < p
+                    e = [i j]
+                    edges = consecutive_pairs(e)
+                    add_edges_from!(meta_g, edges)
 
-#                     if rand(1)[1] < 1 - eta
-#                         err = 0
-#                     else
-#                         err = (n - 1) * rand(1)[1]
-#                     end
-#                     a = pi * (i - j + err) / (n - 1)
-#                     for e in edges
-#                         set_prop!(meta_g, Edge(e), :angle, a)
-#                     end
-#                 end
-#             end
-#         end
-#     end
+                    if rand(1)[1] < 1 - eta
+                        err = 0
+                    else
+                        err = (n - 1) * rand(1)[1]
+                    end
+                    a = pi * (i - j + err) / (n - 1)
+                    for e in edges
+                        set_prop!(meta_g, Edge(e), :angle, a)
+                    end
+                end
+            end
+        end
+    end
 
-#     return meta_g
-# end
+    return meta_g
+end
 
 # Following M. Cucuringu to build comparison graph
 # SYNC-RANK: ROBUST RANKING, CONSTRAINED RANKING AND RANK AGGREGATION VIA EIGENVECTOR AND SDP SYNCHRONIZATION
--- src/laplacians.jl
+++ src/laplacians.jl
@@ -10,15 +10,24 @@ end
 function magnetic_incidence_matrix(
     graph::AbstractGraph; oriented::Bool=true
 )::SparseMatrixCSC{Complex{Float64}}
-    n_v, n_e = nv(graph), ne(graph)
+    B = spzeros(Complex, nv(graph), ne(graph))
+    for (idx_e, e) in enumerate(edges(graph))
+        u = src(e)
+        v = dst(e)
+        angle = get_edge_prop(graph, e, :angle)
+        B[u, idx_e] = exp(0.5 * angle * im)
+        B[v, idx_e] = -exp(-0.5 * angle * im)
+    end
+    return B
+    # n_v, n_e = nv(graph), ne(graph)
 
-    I = vcat(src.(edges(graph)), dst.(edges(graph)))
-    J = vcat(1:n_e, 1:n_e)
+    # I = vcat(src.(edges(graph)), dst.(edges(graph)))
+    # J = vcat(1:n_e, 1:n_e)
 
-    θ = get_edges_prop(graph, :angle, true, 0.0)
-    w = @. exp(im * 0.5 * θ)
-    V = vcat(oriented ? -conj.(w) : w, w)
-    return sparse(I, J, V, n_v, n_e)
+    # θ = get_edges_prop(graph, :angle, true, 0.0)
+    # w = @. exp(im * 0.5 * θ)
+    # V = vcat(oriented ? -conj.(w) : w, w)
+    # return sparse(I, J, V, n_v, n_e)
 end
 
 # todo naming mtsf, csrf
