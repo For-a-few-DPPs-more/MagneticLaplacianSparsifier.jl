ba142502bdd447c9ae130c30cedbe143dfd40fd8
Guillaume Gautier <guillaume.gga@gmail.com>
2021-11-19 21:13:00 +0100

    add subtest for unoriented magnetic incidence

--- test/magnetic_incidence_matrix.jl
+++ test/magnetic_incidence_matrix.jl
@@ -1,6 +1,6 @@
 @testset verbose = true "Magnetic vertex-edge incidence matrix" begin
     g = complete_graph(10)
-    @testset "angle=0 recover oriented=$oriented incidence" for oriented in [true]
+    @testset "angle=0 recover oriented=$oriented incidence" for oriented in [true, false]
         graph = MetaGraph(g, :angle, 0.0)
         B = magnetic_incidence_matrix(graph; oriented=oriented)
         B_theo = Graphs.LinAlg.incidence_matrix(graph, eltype(B); oriented=oriented)
