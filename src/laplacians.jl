8874fa1d899c1b6e389c96af8c0163690fd3e9bb
Michaël Fanuel <mrfanuel@hotmail.fr>
2022-06-02 13:49:42 +0200

    averaging sum of diag matrices

--- src/laplacians.jl
+++ src/laplacians.jl
@@ -41,6 +41,7 @@ function average_sparsifier(
     absorbing_node::Bool=false,
     ust::Bool=false,
 )
+    B = magnetic_incidence(meta_g; oriented=true)
     n = nv(meta_g)
     m = ne(meta_g)
     L = zeros(n, n)
@@ -48,6 +49,12 @@ function average_sparsifier(
     nb_roots = zeros(nb_samples, 1)
     weights = zeros(nb_samples, 1)
     w_tot = 0
+
+    e_weights = ones(m, 1)
+    if weighted
+        e_weights = get_edges_prop(meta_g, :e_weight, true, 1.0)
+    end
+    diag_entries = vec(zeros(m, 1))
 
     for i_sample in 1:nb_samples
         mtsf = multi_type_spanning_forest(rng, meta_g, q; weighted, absorbing_node, ust)
@@ -62,54 +69,25 @@ function average_sparsifier(
         weights[i_sample] = w
 
         w_tot += w
-        sparseB = magnetic_incidence(mtsf; oriented=true)
         ind_e = mtsf_edge_indices(mtsf, meta_g)
+        nb_e = length(ind_e)
+
         if ls === nothing
-            nb_e = length(ind_e)
-            W = I / (nb_e / m)
+            diag_entries[ind_e] += w * ones(nb_e, 1) / (nb_e / m)
         else
-            W = diagm(1 ./ ls[ind_e])
-        end
-        if weighted
-            e_weights = get_edges_prop(meta_g, :e_weight, true, 1.0)
-            W *= diagm(e_weights[ind_e])
+            diag_entries[ind_e] += w * (1 ./ ls[ind_e])
         end
 
-        L = L + w * sparseB' * W * sparseB
+        if weighted
+            diag_entries[ind_e] = e_weights[ind_e] .* diag_entries[ind_e]
+        end
     end
     nb_sampled_cycles = sum(nb_cycles)
     nb_sampled_roots = sum(nb_roots)
 
-    L = L / w_tot
+    L = B' * spdiagm(diag_entries / w_tot) * B
 
     return L, nb_sampled_cycles, nb_sampled_roots, weights
-end
-
-function sample_subgraph_iid(
-    rng::Random.AbstractRNG,
-    meta_g::AbstractMetaGraph,
-    ls::Union{Array,Nothing},
-    batch::Integer,
-)
-    n = nv(meta_g)
-    m = ne(meta_g)
-    subgraph = MetaGraph(n)
-    if ls === nothing
-        ind_rd = rand(rng, 1:m, (batch, 1))
-    else
-        p = vec(ls / sum(ls))
-        ind_rd = rand(rng, Categorical(p), (batch, 1))
-    end
-    all_edges = collect(edges(meta_g))
-    subset_edges = all_edges[ind_rd]
-
-    for e in subset_edges
-        add_edge!(subgraph, e)
-        angle = get_edge_prop(meta_g, e, :angle, true)
-        set_prop!(subgraph, e, :angle, angle)
-    end
-
-    return subgraph
 end
 
 function average_sparsifier_iid(
@@ -120,32 +98,38 @@ function average_sparsifier_iid(
     nb_samples::Integer;
     weighted::Bool=false,
 )
+    B = magnetic_incidence(meta_g; oriented=true)
     n = nv(meta_g)
     m = ne(meta_g)
     L = zeros(n, n)
     w_tot = 0
 
+    e_weights = ones(m, 1)
+    if weighted
+        e_weights = get_edges_prop(meta_g, :e_weight, true, 1.0)
+    end
+
+    diag_entries = vec(zeros(m, 1))
+
     for _ in 1:nb_samples
         subgraph = sample_subgraph_iid(rng::Random.AbstractRNG, meta_g, ls, batch)
         w = 1
         w_tot += w
-        sparseB = magnetic_incidence(subgraph; oriented=true)
         ind_e = mtsf_edge_indices(subgraph, meta_g)
+        nb_e = length(ind_e)
+
         if ls === nothing
-            nb_e = length(ind_e)
-            W = I / (nb_e / m)
+            diag_entries[ind_e] += w * ones(nb_e, 1) / (nb_e / m)
         else
-            W = diagm(1 ./ ls[ind_e])
+            diag_entries[ind_e] += w * (1 ./ ls[ind_e])
         end
 
         if weighted
-            e_weights = edge_weights(meta_g)
-            W *= diagm(e_weights[ind_e])
+            diag_entries[ind_e] = e_weights[ind_e] .* diag_entries[ind_e]
         end
-
-        L = L + w * sparseB' * W * sparseB
     end
-    L = L / w_tot
+
+    L = B' * spdiagm(diag_entries / w_tot) * B
 
     return L
 end
@@ -200,4 +184,31 @@ function optimal_perm(mtsf::AbstractMetaGraph)
     non_cycle_nodes = [roots; flt_branches]
     ind_perm = [non_cycle_nodes; setdiff(1:n_v, non_cycle_nodes)]
     return ind_perm
+end
+
+function sample_subgraph_iid(
+    rng::Random.AbstractRNG,
+    meta_g::AbstractMetaGraph,
+    ls::Union{Array,Nothing},
+    batch::Integer,
+)
+    n = nv(meta_g)
+    m = ne(meta_g)
+    subgraph = MetaGraph(n)
+    if ls === nothing
+        ind_rd = rand(rng, 1:m, (batch, 1))
+    else
+        p = vec(ls / sum(ls))
+        ind_rd = rand(rng, Categorical(p), (batch, 1))
+    end
+    all_edges = collect(edges(meta_g))
+    subset_edges = all_edges[ind_rd]
+
+    for e in subset_edges
+        add_edge!(subgraph, e)
+        angle = get_edge_prop(meta_g, e, :angle, true)
+        set_prop!(subgraph, e, :angle, angle)
+    end
+
+    return subgraph
 end
